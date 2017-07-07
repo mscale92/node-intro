6158ab2d09c912fdef30aa61d709ed68b0761172
Mary <scale-mary@gmail.com>
2017-07-07 17:23:08 -0400

    added a local variable for the google api key

--- workshop.js
+++ workshop.js
@@ -1,12 +1,14 @@
 var request = require('request-promise');
 
+var GOOGLE_KEY = 'AIzaSyAYIHU0RclXqIdgWXFNcOfithtw0RCN3YM';
+
 // Euclidian distance between two points
 function getDistance(pos1, pos2) {
   return Math.sqrt(Math.pow(pos1.lat - pos2.lat, 2) + Math.pow(pos1.lng - pos2.lng, 2));
 }
 
 function getIssPosition() {
-    return request('http://api.open-notify.org/iss-now.json')
+  return request('http://api.open-notify.org/iss-now.json')
   .then(
       function (response) {
         //console.log(response)
@@ -27,7 +29,19 @@ function getIssPosition() {
 }
 
 function getAddressPosition(address) {
+    return request('http://api.open-notify.org/iss-now.json')
+        .then(function (response) {
+                //console.log(response)
+                // Parse as JSON
+                var addressResponseObject = JSON.parse(response)
+                console.log(issResponseObject);
 
+                var addressLatLngObject = {};
+
+                
+                // Return object with lat and lng
+            }
+        )
 }
 
 function getCurrentTemperatureAtPosition(position) {
